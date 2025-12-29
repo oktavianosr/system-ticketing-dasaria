d4196f217b5f7dd530425cec0a2486b2f21ebe2e
vino <oktavianosahru.r@gmail.com>
2025-12-29 19:38:14 +0700

    Initial commit - setup project

---
+++ Backend/database/migrations/2025_12_29_121535_create_ticket_histories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ticket_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ticket_id')->references('id')->on('tickets')->onDelete('cascade');
+            $table->foreignId('changed_by')->references('id')->on('users')->onDelete('cascade');
+            $table->string('field_changed');
+            $table->text('old_value')->nullable();
+            $table->text('new_value')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ticket_histories');
+    }
+};
